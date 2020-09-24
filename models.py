7b06c5cf82c36e6a936c1cb47c02955ca14efc36
Jacob Peplinski <jacobpeplinski@gmail.com>
2020-09-24 15:14:55 -0700

    mobilenetv3 implementation

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Reshape, Input
+from tensorflow.keras.layers import Reshape, Input, SeparableConv2D, Conv2D, Dense
+from mobilenetv3_block import BottleNeck, h_swish
 
 
 def mobilenetv2_96x64_1s(alpha=1.0, embedding_size=12288):
@@ -18,9 +19,38 @@ def mobilenetv2_96x64_1s(alpha=1.0, embedding_size=12288):
     return tf.keras.models.Model(inputs=[model_in], outputs=[net], name='model_v0')
 
 
-def fred_v0_96x64_1s():
+def fred_v0_96x64_1s(out_size=12288):
+    model_in = Input(shape=(64, 96))
+    net = Reshape((64, 96, 1))(model_in)
     pass
 
 
+def keras_mobilenetv3_small(num_classes=12288):
+    net = tf.keras.Sequential([
+        Input(shape=(64, 96)),
+        Reshape((64, 96, 1)),
+        tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), strides=1, padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        BottleNeck(in_size=16, exp_size=16, out_size=16, s=2, is_se_existing=True, NL="RE", k=3),
+        BottleNeck(in_size=16, exp_size=72, out_size=24, s=2, is_se_existing=False, NL="RE", k=3),
+        BottleNeck(in_size=24, exp_size=88, out_size=24, s=1, is_se_existing=False, NL="RE", k=3),
+        BottleNeck(in_size=24, exp_size=96, out_size=40, s=2, is_se_existing=True, NL="HS", k=5),
+        BottleNeck(in_size=40, exp_size=240, out_size=40, s=1, is_se_existing=True, NL="HS", k=5),
+        BottleNeck(in_size=40, exp_size=240, out_size=40, s=1, is_se_existing=True, NL="HS", k=5),
+        BottleNeck(in_size=40, exp_size=120, out_size=48, s=1, is_se_existing=True, NL="HS", k=5),
+        BottleNeck(in_size=48, exp_size=144, out_size=48, s=1, is_se_existing=True, NL="HS", k=5),
+        BottleNeck(in_size=48, exp_size=288, out_size=96, s=2, is_se_existing=True, NL="HS", k=5),
+        BottleNeck(in_size=96, exp_size=576, out_size=96, s=1, is_se_existing=True, NL="HS", k=5),
+        BottleNeck(in_size=96, exp_size=576, out_size=96, s=1, is_se_existing=True, NL="HS", k=5),
+        tf.keras.layers.Conv2D(filters=576, kernel_size=(1, 1), strides=1, padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.AveragePooling2D(pool_size=(4, 6), strides=1),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(num_classes, activation=tf.nn.swish)
+    ])
+    return net
+
+
 if __name__ == "__main__":
-    mobilenetv2_96x64_1s()
+    model = keras_mobilenetv3_small()
+    model.summary()
