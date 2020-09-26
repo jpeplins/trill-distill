b1e0d2d9e320b9839e14bfd86f4575f3fc05f866
Jacob Peplinski <jacobpeplinski@gmail.com>
2020-09-26 15:55:56 -0700

    Improved distillation driver script

--- train_model.sh
+++ train_model.sh
@@ -2,6 +2,35 @@
 set -e
 set -x
 
-python -m train_and_eval -dataset_path "/data2/audioset-speech/tfrecords" -output_path "/data2/audioset-speech/models"
+#########################################
+# A driver script for model distillation.
+#########################################
 
+# PARAMS
+BATCH_SIZE=64
+NUM_EPOCHS=100
+EMBEDDING_SIZE=2048
+LEARNING_RATE=0.1
+TENSORBOARD_PORT=6006
 
+# INPUTS
+DATASET_PATH="/data2/audioset-speech/tfrecords"
+MODEL_NAME="mnetv3_small_${EMBEDDING_SIZE}_v0"
+
+# OUTPUTS
+LOGDIR="/data2/audioset-speech/tensorboard/"
+OUTPUT_PATH="/data2/audioset-speech/models"
+CHECKPOINT_PATH="/data2/audioset-speech/checkpoints"
+
+tensorboard --logdir "${LOGDIR}" --port "${TENSORBOARD_PORT}"
+
+python -m train_and_eval \
+-model_name "${MODEL_NAME}" \
+-dataset_path "${DATASET_PATH}" \
+-output_path "${OUTPUT_PATH}" \
+-checkpoint_path "${CHECKPOINT_PATH}" \
+-log_path "${LOGDIR}" \
+-learning_rate "${LEARNING_RATE}" \
+-num_epochs "${NUM_EPOCHS}" \
+-batch_size "${BATCH_SIZE}" \
+-embedding_size "${BATCH_SIZE}"
