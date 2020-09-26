86155026018dd567b660d5bfce0bd1680e10e8a5
Jacob Peplinski <jacobpeplinski@gmail.com>
2020-09-26 13:40:43 -0700

    tensorboard, model saving, early stopping

--- train_and_eval.py
+++ train_and_eval.py
@@ -1,9 +1,9 @@
-from absl import flags, logging, app
 from input_manager import get_dataset
+from models import distilled_model
+from datetime import datetime
 from tensorflow import keras
-from models import mobilenetv2_96x64_1s, keras_mobilenetv3_tiny
+from absl import flags, app
 import tensorflow as tf
-import numpy as np
 import os
 
 physical_devices = tf.config.list_physical_devices('GPU')
@@ -15,43 +15,72 @@ except:
     pass
 
 FLAGS = flags.FLAGS
+flags.DEFINE_string('model_name', None, 'Give your model a name.')
 flags.DEFINE_string('dataset_path', None, 'Path to collection of TFRecords.')
-flags.DEFINE_string('output_path', None, 'Store model checkpoints and stuff.')
+flags.DEFINE_string('output_path', None, 'Path to save distilled embedding models.')
+flags.DEFINE_string('checkpoint_path', None, 'Path to save training checkpoints.')
+flags.DEFINE_string('log_path', None, 'Path to store logs for tensorboard')
 flags.DEFINE_float('learning_rate', 0.05, 'You know what this does.')
 flags.DEFINE_integer('num_epochs', 50, 'You know what this does.')
 flags.DEFINE_integer('batch_size', 64, 'You know what this does.')
+flags.DEFINE_integer('embedding_size', 2048, 'Size of embedding to distill.')
+flags.DEFINE_float('dropout', 0.0, 'Dropout.')
 
 
-def main(unused_argvs):
+def main(_):
     assert FLAGS.dataset_path
     assert FLAGS.output_path
+    assert FLAGS.checkpoint_path
+    assert FLAGS.log_path
+    assert FLAGS.model_name
 
     train_ds, test_ds = get_dataset(FLAGS.dataset_path, batch_size=FLAGS.batch_size)
 
-    model = keras_mobilenetv3_tiny()
+    embedding_model, distillation_model = distilled_model(
+        embedding_size=FLAGS.embedding_size,
+        dropout=FLAGS.dropout
+    )
 
-    model.compile(
+    distillation_model.compile(
         optimizer=keras.optimizers.Adam(learning_rate=FLAGS.learning_rate),
         loss=keras.losses.MeanSquaredError(),
         metrics=['accuracy']
     )
 
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=FLAGS.output_path,
+        filepath=FLAGS.checkpoint_path,
         save_weights_only=False,
-        monitor='loss',
+        monitor='val_loss',
         mode='auto',
         save_best_only=True)
 
-    history = model.fit(
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss',
+        patience=10,
+        mode='auto',
+    )
+
+    board = tf.keras.callbacks.TensorBoard(
+        log_dir=os.path.join(FLAGS.log_path, datetime.now().strftime("%Y%m%d-%H%M%S")),
+        histogram_freq=0,
+        write_graph=True,
+        write_images=False,
+        update_freq="epoch",
+        profile_batch=2,
+    )
+
+    distillation_model.fit(
         x=train_ds,
         validation_data=test_ds,
         epochs=FLAGS.num_epochs,
         batch_size=FLAGS.batch_size,
-        callbacks=[model_checkpoint_callback]
+        callbacks=[model_checkpoint_callback, early_stopping, board]
     )
 
-    np.savetxt(os.path.join(FLAGS.output_path, "loss.txt"), np.array(history.history['loss']), delimiter=",")
+    tf.keras.models.save_model(
+        embedding_model,
+        os.path.join(FLAGS.output_path, FLAGS.model_name)
+    )
 
 
 if __name__ == '__main__':
